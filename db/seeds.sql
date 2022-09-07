829b4e84161f0b067ac135c2eaa24356081e2c9a
relero90 <relero90@gmail.com>
2022-09-06 18:39:50 -0600

    repaired bug with seeds.sql - both .sql files running as expected

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,9 +15,9 @@ VALUES
 
 INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
 VALUES
-    (1, "Bill", "Bean", 1, 6),
-    (2, "Bernie", "Queen", 3, 4),
-    (3, "Hollis", "Sheen", 2, 5),
-    (4, "Marla", "Mean", 5, NULL),
-    (5, "Matt", "Jean", 4, NULL),
-    (6, "Becca", "Boo", 6, NULL);
+    (1, "Bill", "Bean", 1, NULL),
+    (2, "Bernie", "Queen", 3, NULL),
+    (3, "Hollis", "Sheen", 2, NULL),
+    (4, "Marla", "Mean", 5, 1),
+    (5, "Matt", "Jean", 4, 2),
+    (6, "Becca", "Boo", 6, 3);

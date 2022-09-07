9d5dbbd5a89cadcebb3c836270db6ce9cceda90a
relero90 <relero90@gmail.com>
2022-09-06 18:13:37 -0600

    added switch cases for startQuestion response; wrote logic for displaying departments table from database; bug found with running seeds.sql

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const inquirer = require("inquirer");
+const chalk = require("chalk");
+const cTable = require("console.table");
 const db = require("./assets/JS/connection");
 const { Department, Role, Employee } = require("./assets/JS/constructor");
 
@@ -65,3 +67,56 @@ const addAnEmployeeQuestions = [
     name: "manager",
   },
 ];
+
+// to begin, pass "node index.js start" in the terminal
+function init() {
+  if (process.argv[2] === "start") {
+    console.log(chalk.yellow("Welcome to the employee tracker."));
+
+    promptStart();
+  }
+}
+init();
+
+function promptStart() {
+  inquirer.prompt(startQuestion).then((data) => {
+    switch (data.userAction) {
+      case "View all departments":
+        // console log table of all departments
+        db.query("SELECT * FROM departments", function (err, results) {
+          if (err) {
+            console.log(chalk.red(err));
+          } else {
+            const table = cTable.getTable(results);
+            console.log(chalk.cyan(table));
+          }
+        });
+        break;
+      case "View all roles":
+        // console log table of all roles
+        break;
+      case "View all employees":
+        // console log table of al employees
+        break;
+      case "Add a department":
+        promptForNewDept();
+        break;
+      case "Add a role":
+        promptForNewRole();
+        break;
+      case "Add an employee":
+        promptForNewEmployee();
+        break;
+      case "Update an employee role":
+        // query to UPDATE (see M12 A9)
+        // display updated data to the user?
+        break;
+    }
+  });
+}
+
+function promptForNewDept() {}
+
+function promptForNewRole() {}
+
+function promptForNewEmployee() {}
